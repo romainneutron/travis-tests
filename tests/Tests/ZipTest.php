68de859505d9f42e5b02e55cd54d129d139f3cc5
Romain Neutron <imprec@gmail.com>
2013-05-04 17:28:31 +0200

    Enhance tests

--- tests/Tests/ZipTest.php
+++ tests/Tests/ZipTest.php
@@ -11,11 +11,20 @@ class ZipTest extends PHPUnit_Framework_TestCase
         if (true !== $zip->open($file, ZIPARCHIVE::CREATE)) {
             $this->fail('Unable to create zip file');
         }
-        $zip->addFile(__FILE__, 'script.php');
-        $zip->close();
-        
-        chmod($file, 0760);
+        if (true !== $zip->addFile(__FILE__, 'script.php')) {
+            $this->fail('Unable to add file to zip');
+        }
+        if (true !== $zip->close()) {
+            $this->fail('Unable to close archive');
+        }
 
-        unlink($file);
+        $this->assertFileExists($file);
+
+        if (true !== @chmod($file, 0760)) {
+            $this->fail('Unable to chmod the archive');
+        }
+        if (true !== @unlink($file)) {
+            $this->fail('Unable to unlink the archive');
+        }
     }
 }
